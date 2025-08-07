b7c240a3d42926642b42be810a15ddc799166d0c
zhangkuo <769329489@qq.com>
2025-08-07 17:31:02 +0800

    添加hell-world

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 // src/app/app.routes.ts
 
 import { Routes } from '@angular/router';
+import { HelloWorld } from './pages/hello-world/hello-world';
 
-
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'hello-world',
+    component: HelloWorld,
+  },
+  { path: '', redirectTo: '/hello-world', pathMatch: 'full' },
+];
